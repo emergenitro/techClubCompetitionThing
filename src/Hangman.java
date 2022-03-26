09e09dff5b6063ef3f98245c30ed3766265ae97f
Karthik Sankar <emergenitro@gmail.com>
2022-03-26 20:48:43 +0800

    Couple changes and added functions

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,30 +1,52 @@
 import java.util.Scanner;
 
 public class Hangman {
-    public static void main(String[] args) throws Exception {
-        String arr[] = new String[]{"word", "hello", "test", "random", "help", "arizona", "cut", "somebody", "never", "gonna", "give", "you", "up"};
-        String word = arr[(int)(Math.random() * arr.length)];
-        int wordLength = word.length() - 1;
-        char word_array[] = new char[wordLength];
-        char guessed_word[] = new char[wordLength];
-        int numOfGuesses = 0;
+    private static String arr[] = new String[]{"word", "hello", "test", "random"};
+    private static String word = arr[(int)(Math.random() * arr.length)];
+    private static char word_array[] = new char[word.length()];
+    private static char guessed_word[] = new char[word.length()];
+    private static int numOfGuesses = 0;
+    private static boolean wordGuessed = false;
 
-        // Setting the array "guessed_word" to all underscores
-        for (int i=0; i < word_array.length; ++i){
-            word_array[i] = '_';
-        }
-        for (int j=0; j < word_array.length; ++ j){
-            System.out.print(word_array[j] + " ");
-        }
+    public static void start() {
+        while (numOfGuesses < 7 && !wordGuessed){
+            // Initializing the array "guessed_word" to all underscores
+            for (int i=0; i < word_array.length; ++i){
+                word_array[i] = '_';
+                System.out.print(word_array[i] + " ");
+            }
 
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your guess: ");
-    
-        String str = sc.next(); 
-        for (int j=0;j < str.length(); ++j) {
-            char[] chars = str.toCharArray();
-            if (!Character.isLetter(chars[j])) System.out.print("UNveliaven;le sutpdi");
-        }
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter your guess: ");
         
+            String str = sc.next(); 
+            for (int j=0;j < str.length(); ++j) {
+                char[] chars = str.toCharArray();
+                if (!Character.isLetter(chars[j])){
+                    System.out.print("Kindly re-enter your guess\n\n");
+                    break;
+                } 
+            }
+            System.out.println(checkIfWordContainsGuess(str.charAt(0)));
+            if (!checkIfWordContainsGuess(str.charAt(0))) {
+                numOfGuesses++;
+            } else {
+                updateGuessedWord(str.charAt(0));
+            }
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        start();
+    }
+    public static boolean checkIfWordContainsGuess(char c){
+        for (int i=0; i<word.length();++i){
+            if (word.charAt(i) == c) return true;
+        }
+        return false;
+    }
+    public static void updateGuessedWord(char c){
+        for (int i=0; i < word.length();++i){
+            if (word.charAt(i) == c) word_array[i] = c;
+        }
     }
 }
