2777af2e9e37cc73d0272ccf1036fae00c96748b
Karthik Sankar <emergenitro@gmail.com>
2022-03-26 21:13:02 +0800

    Functioning of the game done

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Hangman {
     private static String arr[] = new String[]{"word", "hello", "test", "random"};
     private static String word = arr[(int)(Math.random() * arr.length)];
     private static char word_array[] = new char[word.length()];
-    private static char guessed_word[] = new char[word.length()];
     private static int numOfGuesses = 0;
-    private static boolean wordGuessed = false;
+    public static String guessedWord = new String();
 
     public static void start() {
-        while (numOfGuesses < 7 && !wordGuessed){
-            // Initializing the array "guessed_word" to all underscores
+        guessedWord = new String(word_array);
+        while (numOfGuesses < 7 && guessedWord.contains("_")){
             for (int i=0; i < word_array.length; ++i){
-                word_array[i] = '_';
                 System.out.print(word_array[i] + " ");
             }
 
-            Scanner sc = new Scanner(System.in);
+            Scanner Sc = new Scanner(System.in);
             System.out.print("Enter your guess: ");
         
-            String str = sc.next(); 
+            String str = Sc.next(); 
             for (int j=0;j < str.length(); ++j) {
                 char[] chars = str.toCharArray();
                 if (!Character.isLetter(chars[j])){
@@ -27,23 +27,32 @@ public class Hangman {
                     break;
                 } 
             }
-            System.out.println(checkIfWordContainsGuess(str.charAt(0)));
             if (!checkIfWordContainsGuess(str.charAt(0))) {
                 numOfGuesses++;
             } else {
                 updateGuessedWord(str.charAt(0));
+                guessedWord = new String(word_array);
             }
         }
     }
     public static void main(String[] args) throws Exception {
+        
+        // Initializing the array "guessed_word" to all underscores
+        for (int i=0; i < word_array.length; ++i){
+            word_array[i] = '_';
+        }
         start();
     }
+
+
     public static boolean checkIfWordContainsGuess(char c){
         for (int i=0; i<word.length();++i){
             if (word.charAt(i) == c) return true;
         }
         return false;
     }
+
+    
     public static void updateGuessedWord(char c){
         for (int i=0; i < word.length();++i){
             if (word.charAt(i) == c) word_array[i] = c;
