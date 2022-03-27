f3bd7b2ea1503755b0f4ad159c122d063a8bec1a
Karthik Sankar <emergenitro@gmail.com>
2022-03-27 14:15:00 +0800

    Bonus and basic done, just have commenting

--- src/Hangman.java
+++ src/Hangman.java
@@ -10,6 +10,7 @@ public class Hangman {
     private static int numOfGuesses;
     private static String guessedWord = new String();
     private static boolean accepted = true;
+    static Scanner Sc = new Scanner(System.in);
 
     public static void restart() {
         // Re-initializing all variables to original, required values
@@ -21,6 +22,7 @@ public class Hangman {
         for (int i=0; i < word_array.length; ++i){
             word_array[i] = '_';
         }
+        img = new String[]{"----------", "|", "|", "|", "|", "|\n|\n----------"};
 
         // Restarting with the above values
         start();
@@ -28,8 +30,11 @@ public class Hangman {
 
     public static void start() {
         guessedWord = new String(word_array);
-        Scanner Sc = new Scanner(System.in);
+        
         while (numOfGuesses < 6 && guessedWord.contains("_")){
+            // Creating some space before printing
+            System.out.println();
+            System.out.println();
             sendText();
             System.out.print("Enter your guess: ");
         
@@ -38,7 +43,7 @@ public class Hangman {
                 char[] chars = str.toCharArray();
                 if (!Character.isLetter(chars[j])){
                     System.out.println("\n\nKindly re-enter your guess. Don't use special chars.\n\n");
-                    accepted = false;
+                    accepted = false; // Check if input is accepted or not, if loop used ahead so that points are not deducted
                     break;
                 } 
             }
@@ -60,21 +65,22 @@ public class Hangman {
             }
             accepted = true;
         }
-        Sc.close();
+        sendText();
+
+        // Winning-Losing messages
+
+
+        System.out.print("\nPlay again (Y/N): ");
+        String a = Sc.next();
+        
+        if (a.equals("Y")) restart();
     }
     public static void main(String[] args) throws Exception {
-        
         // Initializing the array "guessed_word" to all underscores
         for (int i=0; i < word_array.length; ++i){
             word_array[i] = '_';
         }
         start();
-        Scanner Sc2 = new Scanner(System.in);
-        System.out.print("Play again (Y/N): ");
-        String a = Sc2.next();
-        if (a == "Yes"){
-            System.out.println("jd"); 
-        } 
     }
 
 
@@ -93,12 +99,12 @@ public class Hangman {
     }
 
     public static void updateImage(int count){
-        if (count == 1) img[1] += "        0";
-        if (count == 2) img[2] += "      \\";
-        if (count == 3) img[2] += "  /";
-        if (count == 4) img[3] += "        |";
-        if (count == 5) img[4] += "      _/";
-        if (count == 6) img[4] += "\\_";
+        if (count == 1) img[1] = "|        0";
+        if (count == 2) img[1] = "|      \\ 0";
+        if (count == 3) img[1] = "|      \\ 0 /";
+        if (count == 4) img[2] += "        |";
+        if (count == 5) img[3] += "      _/";
+        if (count == 6) img[3] += " \\_";
     }
 
     public static void sendText(){
